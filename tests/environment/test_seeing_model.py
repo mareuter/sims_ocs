cd5c024d3f198ccc78b280a2610136e2b31607c3
Lynne Jones <lynnej@uw.edu>
2018-03-21 16:23:33 -0700

    Updated fwhm_geom values in unit tests and fwhm_system_zenith name

--- tests/environment/test_seeing_model.py
+++ tests/environment/test_seeing_model.py
@@ -25,6 +25,12 @@ class TestSeeingModel(unittest.TestCase):
     def initialize(self, seeing_dbfile=""):
         self.seeing.initialize(self.environment_config, Filters())
 
+    def geom_from_eff(self, fwhm_eff):
+        # See also sims_photUtils:SignalToNoise.py
+        # https://github.com/lsst/sims_photUtils/blob/master/python/lsst/sims/photUtils/SignalToNoise.py
+        fwhm_geom = 0.822 * fwhm_eff + 0.052
+        return fwhm_geom
+
     def compare_seeing(self, seeing_info, truth_fwhm_500_seeing, truth_fwhm_geom_seeing,
                        truth_fwhm_eff_seeing):
         self.assertEqual(seeing_info[0], truth_fwhm_500_seeing)
@@ -37,7 +43,7 @@ class TestSeeingModel(unittest.TestCase):
         self.assertIsNone(self.seeing.seeing_values)
         self.assertIsNone(self.seeing.environment_config)
         self.assertIsNone(self.seeing.filters_config)
-        self.assertIsNone(self.seeing.seeing_fwhm_system_zenith)
+        self.assertIsNone(self.seeing.fwhm_system_zenith)
         self.assertEqual(self.seeing.offset, 0)
 
     def test_information_after_initialization(self):
@@ -46,7 +52,7 @@ class TestSeeingModel(unittest.TestCase):
         self.assertEqual(self.seeing.seeing_dates.size, self.num_original_values)
         self.assertIsNotNone(self.seeing.environment_config)
         self.assertIsNotNone(self.seeing.filters_config)
-        self.assertEqual(self.seeing.seeing_fwhm_system_zenith, 0.39862262855989494)
+        self.assertEqual(self.seeing.fwhm_system_zenith, 0.39862262855989494)
 
     def test_get_seeing(self):
         self.initialize()
@@ -98,18 +104,24 @@ class TestSeeingModel(unittest.TestCase):
         self.initialize()
         self.seeing.get_seeing = mock.MagicMock(return_value=0.0)
         seeing_values = self.seeing.calculate_seeing(self.elapsed_time, 'g', 1.0)
-        self.compare_seeing(seeing_values, 0.0, 0.0, 1.16 * 0.39862262855989494)
+        eff = 1.16 * 0.39862262855989494
+        geom = self.geom_from_eff(eff)
+        self.compare_seeing(seeing_values, 0.0, geom, eff)
 
     def test_calculation_in_g_band(self):
         self.initialize()
         self.seeing.get_seeing = mock.MagicMock(return_value=0.7)
         seeing_values = self.seeing.calculate_seeing(self.elapsed_time, 'g', 1.1)
-        self.compare_seeing(seeing_values, 0.7, 0.74916151132491315, 1.0124922970058186)
+        eff = 1.0124922970058186
+        geom = self.geom_from_eff(eff)
+        self.compare_seeing(seeing_values, 0.7, geom, eff)
 
     def test_calculation_in_z_band_from_internal_info(self):
         self.initialize()
         seeing_values = self.seeing.calculate_seeing(self.elapsed_time, 'z', 1.5)
-        self.compare_seeing(seeing_values, 0.715884983539581, 0.77351383253748662, 1.0886341618872941)
+        eff = 1.0886341618872941
+        geom = self.geom_from_eff(eff)
+        self.compare_seeing(seeing_values, 0.715884983539581, geom, eff)
 
     def test_bad_filter_name(self):
         self.initialize()
